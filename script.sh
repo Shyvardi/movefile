c4ef770a250c6e04eb2b4af319b1436efaaed4e0
Shyvardi <56295870+Shyvardi@users.noreply.github.com>
2023-12-31 17:44:20 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # הגדרת התיקיה המקורית
-source_directory="${MYSOURCE_DIRECTORY}"
+source_directory="${SOURCE_DIRECTORY}"
 
 # הגדרת התיקיה היעד
-destination_directory="${MYDESTINATION_DIRECTORY}"
-# הגדרת זמן המתנה
-sleep_interval="${sleeptime}"
+destination_directory="${DESTINATION_DIRECTORY}"
 
 # שמות התיקיות משנה בתיקית המקור
 subdirectories=("Camera1" "Camera2" "Camera3" "Camera4")
@@ -25,7 +23,13 @@ inotifywait -m -e create --format "%f" -r "$source_directory" | while read -r fi
                 mkdir -p "$full_destination_path"
                 echo "נוצרה תיקיה חדשה: $subdir/$destination_subdirectory_date"
             fi
-            # sleep $sleep_interval
+
+            # בדיקת פעילות בקובץ על ידי fuser
+            while fuser "$source_directory/$subdir/$filename" &> /dev/null; do
+                echo "הקובץ ב-$source_directory/$subdir נמצא בשימוש, ממתין..."
+                sleep 1
+            done
+            
             # העברת הקובץ מהתיקיה המקורית לתיקיה היעד
             mv "$source_directory/$subdir/$filename" "$full_destination_path/"
             echo "הקובץ $filename הועבר בהצלחה לתיקיה: $subdir/$destination_subdirectory_date"
